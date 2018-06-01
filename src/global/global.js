8c8d358bf7d6138b1bfed2ba8b1d542b8cdaf923
Una Bella <369152344@qq.com>
2018-06-01 10:54:36 +0800

    封装报错信息
    about 20

--- src/global/global.js
+++ src/global/global.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Toast } from 'vant';
 // const apiUrl = 'http://192.168.0.109/api/'
 const apiUrl = 'http://ticket.llwell.net/api/'
 export default {
@@ -46,4 +47,51 @@ export default {
       getAccredit();
     }
   },
+  showFailToast(response){
+    console.log(response.msg.code)
+    switch (response.msg.code){
+      case 5000:
+        show('上传小票信息到数据库成功');
+        break;
+      case 5001:
+        show('上传图片失败');
+        break;
+      case 5002:
+        show('上传图片到oss失败');
+        break;
+      case 5003:
+        show('上传小票信息到数据库失败');
+        break;
+      case 5004:
+        show('小票号重复');
+        break;
+      case 5100:
+        show('修改小票信息成功');
+        break;
+      case 5101:
+        show('修改小票信息到数据库失败');
+        break;
+      case 5102:
+        show('未查到票信息');
+        break;
+      case 5103:
+        show('修改的小票状态不对');
+        break;
+      case 5200:
+        show('删除小票信息成功');
+        break;
+      case 5201:
+        show('删除小票信息失败');
+        break;
+      case 6001:
+        show('获取二维码失败');
+        break;
+      default:
+        break;
+    };
+    function show (mes){
+      Toast('出错信息：'+response.msg.code+' '+mes+' '+response.msg.msg);
+    }
+
+  }
 }
